aa42e5732431f8cf2a9bf528c9dd59b9806b652f
dgeorgi <dgeorgi@vmware.com>
2023-12-22 19:07:49 +0200

    Add admin role to the ORM and test it out

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,13 +1,30 @@
 "use client";
 
-import React, { useState } from "react";
-import { QueryClient } from "@tanstack/react-query";
+import React, { PropsWithChildren, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { trpc } from "@/trpc/client";
+import { httpBatchLink } from "@trpc/client";
 
-const Providers = () => {
+const Providers = ({ children }: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient());
-  const [trpcClient] = useState(() => trpc.createClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/trpc`,
+          fetch(url, options) {
+            return fetch(url, { ...options, credentials: "include" });
+          },
+        }),
+      ],
+    })
+  );
 
-  return <div></div>;
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  );
 };
 
 export default Providers;
