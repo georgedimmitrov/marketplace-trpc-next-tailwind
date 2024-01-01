357a974e79436c3fb635610007cb25cfd17df0af
dgeorgi <dgeorgi@vmware.com>
2024-01-01 20:34:02 +0200

    Final touches like metadata. How to add images to a production domain, also a footer

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,7 @@ import { twMerge } from "tailwind-merge";
 import { type ClassValue, clsx } from "clsx";
 import { PRODUCT_CATEGORIES } from "../config";
 import { Product } from "../payload-types";
+import { Metadata } from "next";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -40,4 +41,47 @@ export function extractProductImageUrls(product: Product) {
       return image.url;
     })
     .filter(Boolean) as string[];
+}
+
+export function constructMetadata({
+  title = "DigitalHippo - the marketplace for digital assets",
+  description = "DigitalHippo is an open-source marketplace for high-quality digital goods.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@joshtriedcoding",
+    },
+    icons,
+    metadataBase: new URL("https://somedeployment.url"),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
 }
